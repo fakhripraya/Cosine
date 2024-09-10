7a9522a16d418089b916fe22baca3bcc3ece168f
Fakhri Prayatna <fakhripraya.p@gmail.com>
2024-09-10 19:53:15 +0700

    update deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: true,
+    port: 3000,
+    watch: { usePolling: true },
   },
 });
