76175c19dc369581ef5195712a955bb88130e605
Fakhri Prayatna <fakhripraya.p@gmail.com>
2024-09-16 20:20:05 +0700

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cl/",
   plugins: [react()],
+  base: "./cl",
   server: {
     host: true,
     port: 3000,
