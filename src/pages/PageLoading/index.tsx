4dec01b1e8075b6b38a82856a3cae6e9764d7ddd
Fakhri Prayatna <50196922+fakhripraya@users.noreply.github.com>
2025-04-20 23:48:53 +0700

    update logo

--- src/pages/PageLoading/index.tsx
+++ src/pages/PageLoading/index.tsx
@@ -1,6 +1,6 @@
 import React, { CSSProperties } from "react";
 import "./style.scss";
-import MainLogo from "../../assets/png/pintrail.png";
+import MainLogo from "../../assets/png/transparent-pintrail.png";
 
 interface PageLoadingProps {
   containerStyle?: CSSProperties;
